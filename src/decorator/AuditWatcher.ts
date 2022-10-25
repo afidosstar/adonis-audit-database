c7966e3546fcea77939eb06ab81f3c6ff27356d7
afidos <fiacre.ayedoun@gmail.com>
2022-10-25 12:49:44 +0100

    hotfix: fix bug

--- src/decorator/AuditWatcher.ts
+++ src/decorator/AuditWatcher.ts
@@ -4,30 +4,29 @@ import {
   AuditWatcherDecorator,
 } from "@ioc:Adonis/Addons/AuditDatabase";
 
-const AuditWatcher: AuditWatcherContract = function (): AuditWatcherDecorator {
-  return function (Model) {
-    Model.boot();
-    ["update", "create", "delete"].forEach((event) => {
-      Model.$hooks.add("after", event, async function (entity) {
-        const HttpContext = (await import("@ioc:Adonis/Core/HttpContext"))
-          .default;
-        const Event = (await import("@ioc:Adonis/Core/Event")).default;
-        const { request, route, auth } = await HttpContext.get()!;
-        const user: any = await auth.authenticate().catch(() => null);
-        if (user) {
-          const data: AuditPayload = {
-            route: route,
-            request: request,
-            table: Model.table,
-            data: entity.toJSON(),
-            user: user.toJSON(),
-            event: event,
-          };
-          await Event.emit("adonis:audit:data", data);
-        }
+export const AuditWatcher: AuditWatcherContract =
+  function (): AuditWatcherDecorator {
+    return function (Model) {
+      Model.boot();
+      ["update", "create", "delete"].forEach((event) => {
+        Model.$hooks.add("after", event, async function (entity) {
+          const HttpContext = (await import("@ioc:Adonis/Core/HttpContext"))
+            .default;
+          const Event = (await import("@ioc:Adonis/Core/Event")).default;
+          const { request, route, auth } = await HttpContext.get()!;
+          const user: any = await auth.authenticate().catch(() => null);
+          if (user) {
+            const data: AuditPayload = {
+              route: route,
+              request: request,
+              table: Model.table,
+              data: entity.toJSON(),
+              user: user.toJSON(),
+              event: event,
+            };
+            await Event.emit("adonis:audit:data", data);
+          }
+        });
       });
-    });
+    };
   };
-};
-
-export default AuditWatcher;
